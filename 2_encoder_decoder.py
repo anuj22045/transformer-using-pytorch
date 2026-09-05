58fb6073e97422b2ce1067b6b11305dcb0b2a122
Anuj Kumar <144164000+anuj22045@users.noreply.github.com>
2026-09-05 22:03:57 +0530

    encoder decoder

--- 2_encoder_decoder.py
+++ 2_encoder_decoder.py
@@ -54,5 +54,72 @@ import torch.nn as nn
 # you  -> 3
 
 english_tokens = torch.tensor([[1,2,3]])
+print("English tokens: ", english_tokens)
 
+print("SHape: ", english_tokens.shape)
 
+# ------------------------------------------------------------
+# 2. Token Embedding
+# ------------------------------------------------------------
+
+vocab_size = 10
+embedding_dim = 4
+
+embedding = nn.Embedding(vocab_size, embedding_dim)
+encoder_input = embedding(english_tokens)
+
+print("\n Encoder input shape: ", encoder_input.shape)
+
+# ------------------------------------------------------------
+# 3. Simple Encoder
+# ------------------------------------------------------------
+
+encoder_layer = nn.TransformerEncoderLayer(
+    d_model = embedding_dim, nhead=2, batch_first=True
+)
+
+encoder = nn.TransformerEncoder(
+    encoder_layer, num_layers=1
+)
+
+encoder_output = encoder(encoder_input)
+
+print("\n Encoder output: ", encoder_output)
+
+print("Encoder output shape:")
+print(encoder_output.shape)
+
+# ------------------------------------------------------------
+# 4. French tokens for Decoder
+# ------------------------------------------------------------
+# Suppose:
+# <START> -> 0
+# Je      -> 1
+# t'aime  -> 2
+
+french_tokens = torch.tensor([[0, 1, 2]])
+decoder_input = embedding(french_tokens)
+
+print("\n Decoder input shape: ", decoder_input.shape)
+
+# ------------------------------------------------------------
+# 5. Simple Decoder
+# ------------------------------------------------------------
+
+decoder_layer = nn.TransformerDecoderLayer(
+    d_model = embedding_dim, nhead=2, batch_first=True
+)
+
+decoder = nn.TransformerDecoder(
+    decoder_layer, num_layers=1
+)
+
+decoder_output = decoder(
+    decoder_input, encoder_output
+)
+
+print("\nDecoder output:")
+print(decoder_output)
+
+print("Decoder output shape:")
+print(decoder_output.shape)
